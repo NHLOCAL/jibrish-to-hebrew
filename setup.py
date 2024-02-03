18056b81d402f0f83e511cd50b52ce557f11e2e7
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-02-04 00:27:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(
     version='1.2',
     py_modules=['jibrish_to_hebrew'],  # Specify the name of your Python module
     install_requires=[],  # Add any dependencies if needed
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
