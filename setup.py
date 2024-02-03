a586babea8faa7f72fe70e8ff0678b2bad4c675f
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-02-04 00:21:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jibrish_to_hebrew',
-    version='1.1',
+    version='1.2',
     py_modules=['jibrish_to_hebrew'],  # Specify the name of your Python module
     install_requires=[],  # Add any dependencies if needed
 )
